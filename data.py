a0479795358247749f289da14fe20fe44a664c21
CristyGuzman <csolis@student.ethz.ch>
2022-04-06 12:42:21 +0200

    add preprocessing data to data object

--- data.py
+++ data.py
@@ -7,8 +7,9 @@ from networkx.readwrite import json_graph
 import json
 from tqdm import tqdm
 import argparse
+import scipy.sparse as sp
 import torch
-from utils import load_data
+from utils import load_data, preprocess_graph
 
 
 def json_to_sparse_matrix(file_dir):
@@ -18,12 +19,21 @@ def json_to_sparse_matrix(file_dir):
             json_dict = json.loads(json.load(f))
             print(f'Loading {len(json_dict)} items from file')
             for key, _ in json_dict.items():
-                sparse_adj_matrix = nx.to_scipy_sparse_matrix(json_graph.adjacency_graph(json_dict[key]))
-                x = torch.ones([sparse_adj_matrix.shape[0], 1])
-                edge_index, edge_attrs = from_scipy_sparse_matrix(sparse_adj_matrix)
-                #features = torch.ones([1, adj.shape[0], 1])
-                #adj_features_list.append((adj, features))
-                adj_features_list.append(Data(x=x, edge_index=edge_index))
+                sparse_adj = nx.to_scipy_sparse_matrix(json_graph.adjacency_graph(json_dict[key]))
+                adj_norm = preprocess_graph(sparse_adj)
+                adj_label = sparse_adj + sp.eye(sparse_adj.shape[0])
+                adj_label = torch.FloatTensor(adj_label.toarray())
+                pos_weight = torch.tensor([float(sparse_adj.shape[0] * sparse_adj.shape[0] - sparse_adj.sum()) / sparse_adj.sum()])
+                norm = sparse_adj.shape[0] * sparse_adj.shape[0] / float((sparse_adj.shape[0] * sparse_adj.shape[0] - sparse_adj.sum()) * 2)
+                x = torch.ones([sparse_adj.shape[0], 1])
+                edge_index, edge_attrs = from_scipy_sparse_matrix(sparse_adj)
+                data = Data(x=x, edge_index=edge_index)
+                data.adj_norm = adj_norm
+                data.adj_label = adj_label
+                data.pos_weight = pos_weight
+                data.norm = norm
+                adj_features_list.append(data)
+
     return adj_features_list
 
 
