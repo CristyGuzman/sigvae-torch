dbdf4e513a3b240880aa578d67c3f2ed940c60bf
CristyGuzman <csolis@student.ethz.ch>
2022-04-11 23:48:07 +0200

    create custom dataset

--- data.py
+++ data.py
@@ -51,6 +51,44 @@ def json_to_sparse_matrix(file_dir):
     return adj_features_list
 
 
+import torch
+from torch_geometric.data import InMemoryDataset, download_url
+
+
+class MyOwnDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None, file_dir='/home/csolis/data/ws/sample'):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.file_dir = file_dir
+
+    @property
+    def raw_file_names(self):
+        return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    #def download(self):
+        # Download to `self.raw_dir`.
+    #    download_url(url, self.raw_dir)
+    #    ...
+
+    def process(self):
+        # Read data into huge `Data` list.
+        data_list = json_to_sparse_matrix(self.file_dir)
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+
+
 if __name__ == '__main__':
     from torch_geometric.datasets import TUDataset
     from torch_geometric.loader import DataLoader
