7c5ca08339740d6c7a8ef4bf2664e7d55c5f7901
CristyGuzman <csolis@student.ethz.ch>
2022-04-27 02:09:32 +0200

    add tensorboard, refactor code and use configs

--- train_vgae.py
+++ train_vgae.py
@@ -1,9 +1,12 @@
 import os
 from tqdm import tqdm
+import time
+import sys
 
 import torch
 import torch.nn as nn
 from torch.optim import Adam
+from torch.utils.tensorboard import SummaryWriter
 
 
 from torch_geometric.datasets import Planetoid
@@ -12,6 +15,8 @@ from torch_geometric.utils import train_test_split_edges
 
 from model_vgae import DeepVGAE
 from config_vgae import parse_args
+from vgae_example import create_model_dir
+from configuration import CONSTANTS as C
 
 from data import MyOwnDataset
 from torch_geometric.loader import DataLoader
@@ -23,44 +28,59 @@ args = parse_args()
 
 
 os.makedirs("datasets", exist_ok=True)
-transform = T.Compose([
-    T.NormalizeFeatures(),
-    T.ToDevice(device),
-    T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
-                      split_labels=True, add_negative_train_samples=True),
-])
-#dataset = Planetoid("datasets", args.dataset, transform=transform)
-#data = dataset[0]
-#dataset
-print('Creating training dataset')
-dataset = MyOwnDataset(root=args.train_data_dir, directory=args.train_data_dir,transform=transform)
-loader = DataLoader(dataset, batch_size=args.bs_train)
+def main(config):
 
-args.enc_in_channels = dataset.num_features
-print(f'Args are: {args}')
-print(args.enc_in_channels)
+    transform = T.Compose([
+        T.NormalizeFeatures(),
+        T.ToDevice(C.DEVICE),
+        T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
+                          split_labels=True, add_negative_train_samples=True),
+    ])
+    # dataset = Planetoid("datasets", args.dataset, transform=transform)
+    # data = dataset[0]
+    # dataset
+    print('Creating training dataset')
+    dataset = MyOwnDataset(root=config.train_data_dir, directory=config.train_data_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=config.bs_train)
 
-model = DeepVGAE(args).to(device)
-optimizer = Adam(model.parameters(), lr=args.lr)
+    if config.input_size is None:
+        config.input_size = dataset.num_features
+    print(f'Args are: {args}')
+    print(args.enc_in_channels)
 
+    model = DeepVGAE(config).to(C.DEVICE)
+    optimizer = Adam(model.parameters(), lr=config.lr)
 
+    experiment_id = int(time.time())
+    experiment_name = model.model_name()
+    model_dir = create_model_dir(C.EXPERIMENT_DIR, experiment_id, experiment_name)
 
-#train_data, valid_data, test_data = data
+    config.to_json(os.path.join(model_dir, 'config.json'))
+    # Save the command line that was used.
+    cmd = sys.argv[0] + ' ' + ' '.join(sys.argv[1:])
+    with open(os.path.join(model_dir, 'cmd.txt'), 'w') as f:
+        f.write(cmd)
 
+    writer = SummaryWriter(os.path.join(model_dir, 'logs'))
 
-for epoch in range(args.epoch):
-    for i, abatch in tqdm(enumerate(loader)):
-        train_data, val_data, test_data = abatch
-        all_edge_index = train_data.edge_index
-        model.train()
-        optimizer.zero_grad()
-        loss = model.loss(train_data.x, train_data.pos_edge_label_index, all_edge_index)
-        loss.backward()
-        optimizer.step()
-        if epoch % 2 == 0:
-            model.eval()
-            roc_auc, ap = model.single_test(train_data.x,
-                                            train_data.pos_edge_label_index,
-                                            test_data.pos_edge_label_index,
-                                            test_data.neg_edge_label_index)
-            print("Epoch {} - Loss: {} ROC_AUC: {} Precision: {}".format(epoch, loss.cpu().item(), roc_auc, ap))
+    # train_data, valid_data, test_data = data
+    global_step = 0
+    for epoch in range(config.n_epochs):
+        for i, abatch in tqdm(enumerate(loader)):
+            train_data, val_data, test_data = abatch
+            all_edge_index = train_data.edge_index
+            model.train()
+            optimizer.zero_grad()
+            loss = model.loss(train_data.x, train_data.pos_edge_label_index, all_edge_index)
+            writer.add_scalar('loss/train', loss, global_step)
+            loss.backward()
+            optimizer.step()
+            if epoch % 2 == 0:
+                model.eval()
+                roc_auc, ap = model.single_test(train_data.x,
+                                                train_data.pos_edge_label_index,
+                                                test_data.pos_edge_label_index,
+                                                test_data.neg_edge_label_index)
+                writer.add_scalar('ROC', roc_auc, global_step)
+                writer.add_scalar('ap', ap, global_step)
+                print("Epoch {} - Loss: {} ROC_AUC: {} Precision: {}".format(epoch, loss.cpu().item(), roc_auc, ap))
