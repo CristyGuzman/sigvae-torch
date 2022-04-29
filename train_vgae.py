c606228b7fd67df70d6b06cbc55fbc15e854656c
CristyGuzman <csolis@student.ethz.ch>
2022-04-29 17:15:04 +0200

    define random link split in separate transform func, save model, compute embeddings after training and save

--- train_vgae.py
+++ train_vgae.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm
 import time
 import sys
+import json
 
 import torch
 import torch.nn as nn
@@ -33,9 +34,9 @@ def main(config):
     transform = T.Compose([
         T.NormalizeFeatures(),
         T.ToDevice(C.DEVICE),
-        T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
-                          split_labels=True, add_negative_train_samples=True),
     ])
+    random_link_split = T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
+                          split_labels=True, add_negative_train_samples=True)
     # dataset = Planetoid("datasets", args.dataset, transform=transform)
     # data = dataset[0]
     # dataset
@@ -45,8 +46,9 @@ def main(config):
     file_list = [os.path.join(directory, f) for f in os.listdir(directory)]
     data_list = json_to_sparse_matrix(file_list)
     data_list_transformed = [transform(data) for data in data_list]
-    loader = DataLoader(data_list_transformed, batch_size=config.bs_train)
-
+    data_list_split = [random_link_split(data) for data in data_list_transformed]
+    loader = DataLoader(data_list_split, batch_size=config.bs_train)
+    test_loader = DataLoader(data_list_transformed, batch_size=config.bs_eval)
     if config.input_size is None:
         config.input_size = data_list[0].num_features
 
@@ -56,6 +58,7 @@ def main(config):
     experiment_id = int(time.time())
     experiment_name = model.model_name()
     model_dir = create_model_dir(C.EXPERIMENT_DIR, experiment_id, experiment_name)
+    emb_directory = os.path.join(model_dir, 'embeddings')
     checkpoint_file = os.path.join(model_dir, 'model.pth')
     print('Saving checkpoints to {}'.format(checkpoint_file))
 
@@ -92,29 +95,32 @@ def main(config):
                 writer.add_scalar('ROC', roc_auc, global_step)
                 writer.add_scalar('ap', ap, global_step)
                 print("Epoch {} - Loss: {} ROC_AUC: {} Precision: {}".format(epoch, loss.cpu().item(), roc_auc, ap))
-            #if float(loss) < best_loss:
-            #    best_loss = float(loss)
-            #    torch.save({
-            #        'iteration': i,
-            #        'epoch': epoch,
-            #        'global_step': global_step,
-            #        'model_state_dict': model.state_dict(),
-            #        'optimizer_state_dict': optimizer.state_dict(),
-            #        'total_loss': float(loss),
-            #        'kl_loss': float(kl_loss),
-            #        'recon_loss': float(pos_loss) + float(neg_loss),
-            #    }, checkpoint_file)
+                pos_loss, neg_loss, kl_loss, loss, losses = model.loss(train_data.x, train_data.pos_edge_label_index,
+                                                                       all_edge_index)
+                if float(loss) < best_loss:
+                   best_loss = float(loss)
+                   torch.save({
+                       'iteration': i,
+                       'epoch': epoch,
+                       'global_step': global_step,
+                       'model_state_dict': model.state_dict(),
+                       'optimizer_state_dict': optimizer.state_dict(),
+                       'total_loss': float(loss),
+                       'kl_loss': float(kl_loss),
+                       'recon_loss': float(pos_loss) + float(neg_loss),
+                   }, checkpoint_file)
 
             global_step += 1
+    print("Finsihed training. Get embeddings for all graphs and save them...")
     model.eval()
-    pos_loss, neg_loss, kl_loss, loss, losses = model.loss(train_data.x, train_data.pos_edge_label_index,
-                                                           all_edge_index)
-    torch.save({'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'total_loss': float(loss),
-                'kl_loss': float(kl_loss),
-                'recon_loss': float(pos_loss) + float(neg_loss),
-                }, checkpoint_file)
+    for i, abatch in tqdm(enumerate(test_loader)):
+        latents = model.encode(abatch.x, abatch.edge_index)
+        latents_dict = {'embedding': latents.detach().numpy(),
+                        'batch': abatch.batch}
+        if not os.path.exists(emb_directory):
+            os.makedirs(emb_directory)
+        with open(os.path.join(emb_directory, f'emb_{i}.json'), 'w') as fp:
+            json.dump(latents_dict, fp)
 
 if __name__ == '__main__':
     main(Configuration.parse_cmd())
