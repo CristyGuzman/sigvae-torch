4b88afecdd70d8707aa6962992116664ecc03323
CristyGuzman <csolis@student.ethz.ch>
2022-05-01 18:18:43 +0200

    add batch size to udrs file name

--- get_udr_scores.py
+++ get_udr_scores.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
     #                     include_raw_correlations=True,
     #                     kl_filter_threshold=0.01)
     udrs = compute_udr(model_dir_list, train_data)
-    with open(os.path.join(args.save_dir, 'udrs.txt'), 'w') as f:
+    with open(os.path.join(args.save_dir, f'udrs_{args.batch_size}.txt'), 'w') as f:
         for line in udrs['model_scores']:
             f.write(str(line))
             f.write('\n')
